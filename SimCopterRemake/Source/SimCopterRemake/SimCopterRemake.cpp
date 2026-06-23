874e50e568102b076d242cf26e20c82ba2f80fa7
James Pound <jamespoundiv@gmail.com>
2026-06-23 10:59:42 -0400

    Init repo

---
+++ SimCopterRemake/Source/SimCopterRemake/SimCopterRemake.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimCopterRemake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimCopterRemake, "SimCopterRemake" );
